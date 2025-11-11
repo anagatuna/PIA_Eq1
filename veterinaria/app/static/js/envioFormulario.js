7732d36fc05b37d4a8078a859e89eb27cf8a02ff
anagatuna <altc3456@gmail.com>
2025-11-11 00:31:01 -0600

    diseño formulario y validaciones maxlength

--- veterinaria/app/static/js/envioFormulario.js
+++ veterinaria/app/static/js/envioFormulario.js
@@ -8,14 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
             
             evento.preventDefault(); 
             
-            const fecha = document.getElementById('campoFecha').value;
-            const hoy = new Date().toISOString().split('T')[0];
-            
-            if (fecha < hoy) {
-                alert("La fecha no puede ser anterior al día de hoy.");
-                return;
-            }
-
             alert("Datos recibidos con éxito.");
             miFormulario.reset();
         });
